b67a2c148406b031778270078db01625ed479ad1
Weike Shi <weike.shi@ericsson.com>
2021-10-14 17:06:48 -0400

    test prod baseUrl

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -5,7 +5,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   baseURL = "http://localhost:5000/api";
 } else {
   // production code
-  baseURL = "https://f1-lightsout.herokuapp.com/api";
+  baseURL = "/api";
 }
 export default axios.create({
   baseURL: baseURL,
