b4fb22372a2b3c008e916eef1c282422ee3be7c1
Weike Shi <weike.shi@ericsson.com>
2021-10-14 22:54:09 -0400

    fix redirect to client build index html

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 } else if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
