dc87dd44b5eae02fccbd3ed4663a67fb7b778ae4
Weike Shi <weike.shi@ericsson.com>
2021-10-18 13:34:22 -0400

    typo account

--- client/src/components/sign-out/sign-out.component.jsx
+++ client/src/components/sign-out/sign-out.component.jsx
@@ -46,7 +46,7 @@ const SignOut = ({ currentUser, setCurrentUser, setLeaderboard }) => {
           onClick={handleDelete}
           className="text-xs md:text-sm lg:text-base bg-red-800 py-2 px-4 text-white rounded border border-green focus:outline-none focus:border-green-dark"
         >
-          delete acount
+          delete account
         </button>
       </Link>
     </div>
