2f94725bf295dc34b9aad60ef0337a233c3a2e87
Weike Shi <weikeshih@gmail.com>
2022-09-26 23:54:57 -0400

    Fix footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,37 +1,48 @@
 import React from "react";
 import { SiLinkedin, SiGithub, SiTwitter } from "react-icons/si";
+import { CgWebsite } from "react-icons/cg";
 
 const Footer = () => {
   return (
     <div className="absolute inset-x-0 bottom-10">
-      <div className="text-xs md:text-sm text-center">create by</div>
-      <div className="text-xs md:text-sm flex justify-center font-black gap-x-1">
+      <div className="text-xs md:text-sm text-center">Created by</div>
+      <div className="text-xs md:text-sm flex justify-center items-center gap-x-1">
+        <a
+          href="https://weike-shi.vercel.app/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          @ <span className="hover:underline">Weike Shi</span>
+        </a>
+        <CgWebsite />
+      </div>
+      <div className="text-xs md:text-sm flex justify-center items-center gap-x-1">
         <a
           href="https://www.linkedin.com/in/weike-shi/"
           rel="noopener noreferrer"
           target="_blank"
         >
-          @ weike shi
+          @ <span className="hover:underline">LinkedIn</span>
         </a>
         <SiLinkedin />
       </div>
-      <div className="text-xs md:text-sm flex justify-center font-black gap-x-1">
+      <div className="text-xs md:text-sm flex justify-center items-center gap-x-1">
         <a
-          href="https://github.com/WeikeShi0730/lights-out-firebase"
+          href="https://github.com/WeikeShi0730/worldle"
           rel="noopener noreferrer"
           target="_blank"
         >
-          @ github
+          @ <span className="hover:underline">Github</span>
         </a>
         <SiGithub />
       </div>
-      <div className="text-xs md:text-sm text-center flex justify-center font-black gap-x-1">
+      <div className="text-xs md:text-sm text-center flex justify-center items-center gap-x-1">
         <a
           href="https://twitter.com/vicshi97"
           rel="noopener noreferrer"
           target="_blank"
         >
-          @ twitter
+          @ <span className="hover:underline">Twitter</span>
         </a>
         <SiTwitter />
       </div>
